5c4dc275d7226e5eafceab2412bc6d9f6c8b0508
Nastaran Rajabi <rajabinastaran81@gmail.com>
2025-01-10 18:08:09 +0330

    getting user input

---
+++ file1.java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        
+}
